53277ae5470e1f4b2855f05ef74442381bf89dac
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-08-27 16:06:25 +0500

    all it works

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <p className="text-8xl">How are you</p>
+      <p className="text-8xl text-red-500">Hello</p>
     </div>
   );
 }
