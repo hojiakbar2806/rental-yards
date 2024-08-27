52f6d2ca0027eabc8cd6b2575ea5cf6fd1dbf638
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-08-27 11:09:22 +0500

    Add frontend and backend code

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <p className="text-8xl">Hello</p>
+    </div>
+  );
+}
